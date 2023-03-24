233741946d91bc0c6b7ef6dbc28a069064319e2d
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2023-03-24 17:43:32 +0600

    feat: state for create todo

--- src/components/createTodo/CreateTodo.jsx
+++ src/components/createTodo/CreateTodo.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import css from "./CreateTodo.module.css";
 
 const CreateTodo = () => {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    alert(inputValue);
+  }
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
   return (
-    <div className={css.wrapper}>
-      <input className="mainInput" placeholder="Add some todo" type="text" />
+    <form onSubmit={submit} className={css.wrapper}>
+      <input value={inputValue} onChange={handleChange} className="mainInput" placeholder="Add some todo" type="text" />
       <button className="mainBtn">+Add</button>
-    </div>
+    </form>
   );
 };
 
